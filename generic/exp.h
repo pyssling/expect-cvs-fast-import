6e639066eb4f8ef784d98699ea164faed09fb4d0
davygrvy <davygrvy>
2002-02-10 09:03:41 +0000

    fixed some preprocessor logic that was in error.

--- generic/exp.h
+++ generic/exp.h
@@ -22,7 +22,7 @@
  *	    http://expect.sf.net/
  *	    http://bmrc.berkeley.edu/people/chaffee/expectnt.html
  * ----------------------------------------------------------------------------
- * RCS: @(#) $Id: exp.h,v 1.1.4.1 2002/02/10 02:58:52 davygrvy Exp $
+ * RCS: @(#) $Id: exp.h,v 1.1.4.2 2002/02/10 08:40:54 davygrvy Exp $
  * ----------------------------------------------------------------------------
  */
 
@@ -112,10 +112,13 @@
 #ifndef TCL_EXTERN
 #   undef DLLIMPORT
 #   undef DLLEXPORT
-#   if defined(__WIN32__) && (defined(_MSC_VER) || (defined(__GNUC__) && defined(__declspec))) || (defined(MAC_TCL) && FUNCTION_DECLSPEC)
+#   if (defined(__WIN32__) &&  (defined(_MSC_VER) || (__BORLANDC__ >= 0x0550) || \
+	    (defined(__GNUC__) && defined(__DECLSPEC_SUPPORTED)))) || \
+	    (defined(MAC_TCL) && FUNCTION_DECLSPEC)
 #	define DLLIMPORT __declspec(dllimport)
 #	define DLLEXPORT __declspec(dllexport)
 #   elif defined(__BORLANDC__)
+#	define OLD_BORLAND 1
 #	define DLLIMPORT __import
 #	define DLLEXPORT __export
 #   else
@@ -132,9 +135,9 @@
 #	define TCL_CPP
 #   endif
     /*
-     * Borland requires the attributes be placed after the return type.
+     * Pre 5.5 Borland requires the attributes be placed after the return type.
      */
-#   ifdef __BORLANDC__
+#   if OLD_BORLAND
 #	define TCL_EXTERN(rtnType) extern TCL_CPP rtnType TCL_STORAGE_CLASS
 #   else
 #	define TCL_EXTERN(rtnType) extern TCL_CPP TCL_STORAGE_CLASS rtnType
