35887269faee1ff968405141a74e6d96650ba6f5
davygrvy <davygrvy>
2002-03-11 06:56:02 +0000

    added missing file header comments

--- win/expWinSpawnPipeClient.cpp
+++ win/expWinSpawnPipeClient.cpp
@@ -1,5 +1,32 @@
-#include "expWinInt.h"
+/* ----------------------------------------------------------------------------
+ * expWinSpawnPipeClient.cpp --
+ *
+ *	Simple pipes as our IPC mechanism.
+ *
+ * ----------------------------------------------------------------------------
+ *
+ * Written by: Don Libes, libes@cme.nist.gov, NIST, 12/3/90
+ * 
+ * Design and implementation of this program was paid for by U.S. tax
+ * dollars.  Therefore it is public domain.  However, the author and NIST
+ * would appreciate credit if this program or parts of it are used.
+ * 
+ * Copyright (c) 1997 Mitel Corporation
+ *	work by Gordon Chaffee <chaffee@bmrc.berkeley.edu> for the WinNT port.
+ *
+ * Copyright (c) 2001-2002 Telindustrie, LLC
+ *	work by David Gravereaux <davygrvy@pobox.com> for any Win32 OS.
+ *
+ * ----------------------------------------------------------------------------
+ * URLs:    http://expect.nist.gov/
+ *	    http://expect.sf.net/
+ *	    http://bmrc.berkeley.edu/people/chaffee/expectnt.html
+ * ----------------------------------------------------------------------------
+ * RCS: @(#) $Id: expWinSlaveTrapDbg.cpp,v 1.1.4.4 2002/03/10 01:02:37 davygrvy Exp $
+ * ----------------------------------------------------------------------------
+ */
 
+#include "expWinInt.h"
 
 SpawnPipeClient::SpawnPipeClient(const char *name, CMclQueue<Message> &_mQ)
     : mQ(_mQ)
@@ -13,11 +40,14 @@ void
 SpawnPipeClient::Write(Message &what)
 {
     DWORD dwWritten;
+    HANDLE where;
 
     switch (what.mode) {
     case Message::TYPE_NORMAL:
-	WriteFile(hStdOut, what.bytes, what.length, &dwWritten, 0L);
+	where = hStdOut;
     case Message::TYPE_ERROR:
-	WriteFile(hStdErr, what.bytes, what.length, &dwWritten, 0L);
+	where = hStdErr;
     }
+
+    WriteFile(where, what.bytes, what.length, &dwWritten, 0L);
 }
--- win/expWinUtils.cpp
+++ win/expWinUtils.cpp
@@ -1,3 +1,31 @@
+/* ----------------------------------------------------------------------------
+ * expWinUtils.cpp --
+ *
+ *	Misc stuff.
+ *
+ * ----------------------------------------------------------------------------
+ *
+ * Written by: Don Libes, libes@cme.nist.gov, NIST, 12/3/90
+ * 
+ * Design and implementation of this program was paid for by U.S. tax
+ * dollars.  Therefore it is public domain.  However, the author and NIST
+ * would appreciate credit if this program or parts of it are used.
+ * 
+ * Copyright (c) 1997 Mitel Corporation
+ *	work by Gordon Chaffee <chaffee@bmrc.berkeley.edu> for the WinNT port.
+ *
+ * Copyright (c) 2001-2002 Telindustrie, LLC
+ *	work by David Gravereaux <davygrvy@pobox.com> for any Win32 OS.
+ *
+ * ----------------------------------------------------------------------------
+ * URLs:    http://expect.nist.gov/
+ *	    http://expect.sf.net/
+ *	    http://bmrc.berkeley.edu/people/chaffee/expectnt.html
+ * ----------------------------------------------------------------------------
+ * RCS: @(#) $Id: expWinSpawnPipeClient.cpp,v 1.1.2.2 2002/03/11 06:53:39 davygrvy Exp $
+ * ----------------------------------------------------------------------------
+ */
+
 #include "expWinUtils.hpp"
 #include <ctype.h>
 #include <string.h>
